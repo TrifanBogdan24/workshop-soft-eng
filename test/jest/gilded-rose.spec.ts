e2d4d2c7228ed4ba63dcba4c3a15d3ea1f910fd2
TrifanBogdan24 <trifanbogdan24@gmail.com>
2024-12-03 17:51:58 +0200

    I trieed...

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,7 +1,10 @@
 import { Item, GildedRose } from '@/gilded-rose';
+import * as assert from "node:assert";
+
+
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
+  it('Naming', () => {
     // Arrange
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
 
@@ -21,5 +24,49 @@ describe('Gilded Rose', () => {
 
     // Assert
     expect(items[0].quality).toBe(0);
-  })
+  });
+
+
+  describe('Common item', () => {
+    describe('Quality is never negative', () => {
+      it('Test 1', () => {
+        const gildedRose = new GildedRose([new Item('Rose', 10, 10)]);
+        const items = gildedRose.updateQuality();
+        const updatedQuality= items[0].quality;
+        expect(updatedQuality).toBe(0);
+      });
+
+      it('Test 2', () => {
+        const gildedRose = new GildedRose([new Item('Red Hat', 1000, 10)]);
+        const items = gildedRose.updateQuality();
+        const quality= items[0].quality;
+        expect(quality).toBe(0);
+      });
+
+
+      describe('Decreasing quality before data passed', () => {
+        it('Test 1', () => {
+          const gildedRose = new GildedRose([new Item('Rose', 10, 5)]);
+          const items = gildedRose.updateQuality();
+          const updatedQuality= items[0].quality;
+          expect(updatedQuality).toBe(5);
+        });
+      })
+    })
+  });
+
+  describe('Aged Brie', () => {
+
+  });
+
+  describe('Sulfuras', () => {
+
+  });
+
+  describe('Backstage passes', () => {
+
+  });
+
+
+
 });
